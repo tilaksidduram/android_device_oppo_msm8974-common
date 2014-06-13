4b40e74f199f42455344f7066c01fd2c0ad0cb01
SferaDev <sferadev@gmail.com>
2014-06-13 07:52:12 -0400

    msm8974: Track oppo/common
    Change-Id: I8ee1edb46b01d144e2f01e0acd67b00ed0c4801c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# inherit from Oppo common
+-include device/oppo/common/BoardConfigCommon.mk
+
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := device/oppo/msm8974-common/include
 
